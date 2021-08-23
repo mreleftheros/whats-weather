7276eddba29e0549660cc8fc9883918bfb1bab0a
eleftheros <eleftheros_all@protonmail.com>
2021-08-23 09:39:28 +0300

    Add async getWeather function

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,6 @@
 const key = "9z7d1E0QZA81FYbJEuXhaZT4kINFeZbW";
 
-// async function to get city
+// async function to fetch the city that was passed
 const getCity = async city => {
   const urlBase = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const urlQuery = `?q=${city}&apikey=${key}`;
@@ -10,3 +10,16 @@ const getCity = async city => {
 
   return data[0];
 };
+
+// async function to fetch the weather of the given city key
+const getWeather = async cityKey => {
+  const urlBase = `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}`;
+  const urlQuery = `?apikey=${key}`;
+
+  const response = await fetch(urlBase + urlQuery);
+  const data = await response.json();
+
+  console.log(data);
+};
+
+getWeather(186405);
