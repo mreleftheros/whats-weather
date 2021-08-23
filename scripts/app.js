966bb82cef7f02e0d200eedef331408f6914685a
eleftheros <eleftheros_all@protonmail.com>
2021-08-23 10:36:44 +0300

    Add 2 images in assets folder

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,16 @@
 const cityForm = document.getElementById("cityForm");
+const cityCard = document.getElementById("cityCard");
+const timeImg = document.getElementById("timeImg");
+
+// function that updates the card UI with given data
+const updateCardUI = data => {
+  // update the image with day or night
+  let imgSrc = data.isDay ? 
+
+  let html = `
+
+  `;
+};
 
 // function that handles submit event on cityForm
 const handleCityForm = e => {
@@ -6,7 +18,9 @@ const handleCityForm = e => {
 
   let cityInput = e.currentTarget.city.value.trim();
 
-  updateCityAndWeather(cityInput);
+  updateCityAndWeather(cityInput)
+    .then(data => updateCardUI(data))
+    .catch(err => console.log(err));
 
   e.currentTarget.reset();
 };
